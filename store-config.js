716818c7ca3e728320412188c2bfa83bbc7f5a7d
mazharmnzoor4227-beep <mazharmnzoor4227@gmail.com>
2026-09-06 19:08:54 +0500

    Add store configuration for Clawora Beauty

---
+++ store-config.js
@@ -0,0 +1,12 @@
+window.STORE_CONFIG = {
+  storeName: "Clawora Beauty",
+  currency: "PKR",
+
+  whatsappNumber: "923422271226",
+
+  supportEmail: "",
+  instagram: "",
+
+  deliveryText: "Delivery across Pakistan",
+  codText: "Cash on Delivery available"
+};
